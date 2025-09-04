f580ab35e5d70935f58440ff2077f562e19537bd
Muhammad-Waqar022 <ranawaqar4343@gmail.com>
2025-09-04 12:31:39 +0500

    initial commit

--- contextApi/project2/src/App.jsx
+++ contextApi/project2/src/App.jsx
@@ -2,8 +2,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { ThemeProvider } from './contexts/theme'
-import ThemeBtn from './components/ThemeBtn'
-import Card from './components/Card'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
