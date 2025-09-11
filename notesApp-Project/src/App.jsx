3fe31b0920ef0886df5f222004968bb297ad76fd
Muhammad-Waqar022 <ranawaqar4343@gmail.com>
2025-09-11 12:26:05 +0500

    added code

--- notesApp-Project/src/App.jsx
+++ notesApp-Project/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import NotesList from './Components/NotesList'
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [notes, setNotes] =useState([
+    {
+    id:nanoid(),
+    text: "This is my first note!",
+    date: "5/04/2025"
+  },
+    {
+    id:nanoid(),
+    text: "This is my Second note!",
+    date: "10/04/2025"
+  },
+    {
+    id:nanoid(),
+    text: "This is my Third note!",
+    date: "16/04/2025"
+  },
+    {
+    id:nanoid(),
+    text: "This is my Fourth note!",
+    date: "17/04/2025"
+  },
+    {
+    id:nanoid(),
+    text: "This is my Fifth note!",
+    date: "25/04/2025"
+  },
+    {
+    id:nanoid(),
+    text: "This is my new note!",
+    date: "30/04/2025"
+  },
+])
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+     <NotesList notes={notes}/>
+    </div>
   )
 }
 
